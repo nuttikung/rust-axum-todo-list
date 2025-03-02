60723f95147d2a30b8ddfc81c9c492c5751c9226
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-03-02 23:16:50 +0700

    feat: group route to /api

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use rust_axum_todo_list::setting::Setting;
 
 #[tokio::main]
 async fn main(){
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    // region :      --- Router Constant
+    let api_routes = Router::new().merge(route_todo());
+    // Make it nested as /api/*
+    let router = Router::new().nest("/api", api_routes);
+    // end region :  --- Router Constant
 
     // region :      --- Load Setting
     let setting = Setting::new().unwrap();
@@ -17,8 +21,15 @@ async fn main(){
     let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
     println!("--> LISTENING on {:?} \n", listener.local_addr());
     // region : --------start server--------
-    axum::serve(listener, routes_hello.into_make_service()).await.unwrap();
+    axum::serve(listener, router.into_make_service()).await.unwrap();
 }
+
+// region :      --- Route Hello
+fn route_todo() -> Router {
+    return Router::new()
+        .route("/hello", get(handler_hello))
+}
+// end region :  --- Route Hello
 
 // region :      --- Handle Hello
 async fn handler_hello() -> impl IntoResponse {
