879e224bb65be2e5a5df24adf086628a4b174da8
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-03-03 23:22:38 +0700

    feat(compression): add tower and tower http for compression

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
-
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{Json, Router, routing::get};
 use rust_axum_todo_list::setting::Setting;
+use serde_json::{Value, json};
+use tower::ServiceBuilder;
+use tower_http::{compression::CompressionLayer, decompression::RequestDecompressionLayer};
 
 #[tokio::main]
-async fn main(){
+async fn main() {
     // region :      --- Router Constant
     let api_routes = Router::new().merge(route_todo());
     // Make it nested as /api/*
-    let router = Router::new().nest("/api", api_routes);
+    let router = Router::new().nest("/api", api_routes).layer(
+        ServiceBuilder::new()
+            .layer(RequestDecompressionLayer::new())
+            .layer(CompressionLayer::new()),
+    );
     // end region :  --- Router Constant
 
     // region :      --- Load Setting
     let setting = Setting::new().unwrap();
     let port: String = setting.server.port.to_string();
     let host: String = String::from("127.0.0.1");
-    let address = format!("{}:{}", host, &port ) ;
+    let address = format!("{}:{}", host, &port);
     // end region :  --- Load Setting
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
     println!("--> LISTENING on {:?} \n", listener.local_addr());
     // region : --------start server--------
-    axum::serve(listener, router.into_make_service()).await.unwrap();
+    axum::serve(listener, router.into_make_service())
+        .await
+        .unwrap();
 }
 
 // region :      --- Route Hello
 fn route_todo() -> Router {
-    return Router::new()
-        .route("/hello", get(handler_hello))
+    return Router::new().route("/hello", get(handler_hello));
 }
 // end region :  --- Route Hello
 
 // region :      --- Handle Hello
-async fn handler_hello() -> impl IntoResponse {
-    return "Hello World!";
+async fn handler_hello() -> Json<Value> {
+    Json(json!({ "data": 42 }))
 }
 // end region :  --- Handle Hello
